a601d20d2ce4acdae386d43a9dae88d2b1c1c71a
Andres Zapata <azo_67@hotmail.es>
2019-11-04 13:39:16 -0500

    minor change

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import Login from './pages/Login.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarX from './pages/NavbarX.jsx';
 
@@ -8,12 +9,10 @@ const App = () => (
   <div>
     <NavbarX/>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
+    <Login />
     <Info />
   </div>
 );
-
-
 
 export default App;
 
---
+++ imports/ui/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Row, Col, FormGroup, ControlLabel, Button } from 'react-bootstrap';
+// import OAuthLoginButtons from '../../components/OAuthLoginButtons/OAuthLoginButtons';
+
+export default class Login extends Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    componentDidMount() {
+      const component = this;
+    }
+
+ 
+  
+    render() {
+        return (
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <h1>Register</h1>
+                  
+              </div>
+            </div>
+          )
+  }
+}
