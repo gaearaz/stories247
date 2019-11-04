db665223b322a32853d2515dde26e369af659da8
johann <jayc.7447@gmail.com>
2019-11-04 16:19:59 -0500

    NavBar xd

--- imports/ui/pages/NavbarX.jsx
+++ imports/ui/pages/NavbarX.jsx
@@ -4,22 +4,51 @@ import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import FormControl from 'react-bootstrap/FormControl'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import '../css/navbar.css'
+import NavItem from 'react-bootstrap/NavItem';
 
 export default class NavbarX extends React.Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar>
+      <Navbar className="nav-x" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <Button className="btn-nav" variant="warning">
+            24/7
+          </Button>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#write">
+            <Button className="btn-nav" variant="warning">
+              Escribir
+            </Button>
+          </Nav.Link>
+          <NavDropdown className="dropdown-nav" title="Categorias " >
+            <pre> <b>  Aventurarme con...</b></pre>
+            <NavDropdown.Header />
+            <NavDropdown.Item href="#cat-action">Acción</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-fantasy">Fanatasía</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-romance">Romance</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-historical-fiction">Ficción historica</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-horror">Horror</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-comedy">Comedia</NavDropdown.Item>
+            <NavDropdown.Item href="#cat-poesy">Poesía</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Otros</NavDropdown.Item>
+          </NavDropdown>
+          
+
+        </Nav>
+        <FormControl type="text" placeholder="Search" className="form-nav"/>    
+        <Nav className="ml-auto">
+        <Button className="btn-nav" variant="warning">
+          
+            <b>Acceder</b>
+          </Button>
+          
+        </Nav>
+        
+
+      </Navbar>
     );
   }
 }
