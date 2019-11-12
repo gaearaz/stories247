f76e901c45ecd62cfdfd02dae9899f61fd9585fd
gaearaz <azo_67@hotmail.es>
2019-11-12 07:59:08 -0500

    minor changes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarX from './pages/NavbarX.jsx';
 
+
 const App = () => (
   <div>
     <NavbarX/>
--- imports/ui/pages/Profile.jsx
+++ imports/ui/pages/Profile.jsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import sharedStyles from './css/sharedStyles.css'
+import loginStyles from '../css/login.css';
+import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import NavbarX from './pages/NavbarX.jsx';
+
+const tableStyles = {...styles, ...sharedStyles }
+
+export default class Profile extends React.Component {
+    
+    
+    render(){
+        <NavbarX/>
+
+    }
+
+}
+
+
+// https://github.com/css-modules/css-modules
+//export default CssMudule(Profile, {tableStyles})
